302ed5f4cccdfc35faa14af5184424e6b64d0e2b
Juan Thomás Ángel Montilla <juan.thomas.angel@gmail.com>
2020-04-01 00:36:28 -0400

    subiendo cambios de api

--- common/models/provincia.js
+++ common/models/provincia.js
@@ -1,5 +1,25 @@
 'use strict';
 
 module.exports = function(Provincia) {
+    Provincia.getProvinciasporPost = function(formularioEvaluacion,cb) {
+        console.log(formularioEvaluacion);
+            Provincia.find({}, function(err, result) {
+                if (err) {
+                    // console.log(Provincia.find({}));
+                    console.log(err);
+                } else {
+                    cb(err, result);
+                }
+                // console.log(result);
+            });
+        }
 
+    Provincia.remoteMethod(
+        'getProvinciasporPost', {
+            http: { path: '/getProvinciasporPost', verb: 'post' },
+            accepts:{ arg: 'formularioBloqueado', type: 'object', required: false, http: { source: 'body' }},
+            returns: { arg: 'data', type: 'object', root: true },
+            description: 'Get Provincias'
+        }
+    );
 };
