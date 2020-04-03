5fe6de94dd2198f50d14492a2644086337eb2609
Juan Thomás Ángel Montilla <juan.thomas.angel@gmail.com>
2020-04-02 22:37:33 -0400

    subiendo articulos activos papolo

--- .history/common/models/provincia_20200401004337.js
+++ .history/common/models/provincia_20200401004337.js
@@ -16,37 +16,12 @@ module.exports = function(Provincia) {
             });
         }
 
-        Provincia.getArticulosPorPost = function(provinciaId, cb) {
-            console.log(provinciaId);
-            Provincia.app.models.articulo.find({provinciaId: provinciaId}, function(err, result) {
-                if (err) {
-                    // console.log(Provincia.find({}));
-                    console.log(err);
-                } else {
-                    let articulos = {};
-                    articulos.articulos = result;
-                    cb(err, articulos);
-                }
-                // console.log(result);
-            });
-        }
-
-        // FormularioEvaluacion.app.models.Area
-
     Provincia.remoteMethod(
         'getProvinciasporPost', {
             http: { path: '/getProvinciasporPost', verb: 'post' },
             accepts:{ arg: 'formularioBloqueado', type: 'object', required: false, http: { source: 'body' }},
             returns: { arg: 'data', type: 'object', root: true },
             description: 'Get Provincias'
-        }
-    );
-    Provincia.remoteMethod(
-        'getArticulosPorPost', {
-            http: { path: '/getArticulosPorPost', verb: 'post' },
-            accepts:{ arg: 'provinciaId', type: 'number', required: true},
-            returns: { arg: 'data', type: 'object', root: true },
-            description: 'Get  articulos por provincia'
         }
     );
 };
